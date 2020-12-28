63d987d3c8b1f72dc7d71691ea32e9c0efe2263d
HanxingZhang <zhanghanxing026@gmail.com>
2020-12-27 20:07:20 -0500

    OOD-CashRegister change input

--- OOD/src/main/java/CashRegister.java
+++ OOD/src/main/java/CashRegister.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CashRegister {
@@ -58,17 +60,33 @@ public class CashRegister {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner fileScanner = (args.length > 0) ? new Scanner(new File(args[0]))
                 : new Scanner(System.in);
-
-        while (fileScanner .hasNextLine()) {
-            String line = fileScanner.nextLine();
-            if(!line.equalsIgnoreCase("")) {
-                String elements[] = line.split(";");
-                float price = Float.parseFloat(elements[0]);
-                float cash = Float.parseFloat(elements[1]);
-                System.out.println(findChange(price, cash));
+        List<String> tokens = new ArrayList<>();
+        int line = 0;
+        while (fileScanner.hasNextLine()) {
+            String read = fileScanner.nextLine();
+            if(read == null || read.isEmpty() || line >= 2){ //if the line is empty
+                break;  //exit the loop
+            }
+            if(!read.equalsIgnoreCase("")){
+                tokens.add(read);
+                line++;
             }
         }
         fileScanner.close();
+        System.out.println(tokens.get(0));
+        float price = Float.parseFloat(tokens.get(0));
+        float cash = Float.parseFloat(tokens.get(1));
+        System.out.println(findChange(price, cash));
+
+//        while (fileScanner .hasNextLine()) {
+//            String line = fileScanner.nextLine();
+//            if(!line.equalsIgnoreCase("")) {
+//                String elements[] = line.split(";");
+//                float price = Float.parseFloat(elements[0]);
+//                float cash = Float.parseFloat(elements[1]);
+//                System.out.println(findChange(price, cash));
+//            }
+//        }
     }
 
 }
